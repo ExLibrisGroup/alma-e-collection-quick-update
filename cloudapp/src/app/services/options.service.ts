6248024b2f1d76037b04d2863ca83f18e7ada01e
Josh Weisman <josh.m.weisman@gmail.com>
2020-07-20 14:40:29 +0300

    Small refactoring
    1. Add counter to e-collection list
    2. Refactor options from other APIs

--- cloudapp/src/app/services/options.service.ts
+++ cloudapp/src/app/services/options.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Options } from '../models/options';
 import { Observable, iif, of, forkJoin } from 'rxjs';
 import { CloudAppRestService, Request } from '@exlibris/exl-cloudapp-angular-lib';
-import { tap, switchMap, map, concatMap } from 'rxjs/operators';
-import _ from 'lodash';
+import { tap, switchMap, map } from 'rxjs/operators';
+import { camelCase, cloneDeep} from 'lodash';
 import { CodeTable, Licenses, IntegrationProfiles } from '../models/alma';
 
 @Injectable()
@@ -14,7 +14,19 @@ export class OptionsService {
     'CollectionAccessType',
     'CounterPlatform',
     'BooleanYesNo'
-  ]
+  ];
+  private otherApis = {
+    Proxies: {
+      uri: '/conf/integration-profiles?type=PROXY_DEFINITION',
+      code: 'code',
+      desc: i => i.code.concat(i.description ? ` (${i.description})` : '')
+    },
+    Licenses: {
+      uri: '/acq/licenses',
+      code: 'code',
+      desc: i => i.name
+    }
+  }
   private _options: Options;
 
   constructor(
@@ -30,26 +42,24 @@ export class OptionsService {
         this.codes.map(key=>this.restService.call<CodeTable>(`/conf/code-tables/${key}`))
       )
       .pipe(
-        tap(results=> {
+        tap(results => {
           this._options = new Options();
           results.forEach(table => 
-            this._options[_.camelCase(table.name)] = 
+            this._options[camelCase(table.name)] = 
               table.row.map(r=>({code: r.code, desc: r.description}))
               .sort(sort('desc'))
           )
         }),
         /* Other APIs */
-        switchMap(()=>forkJoin([
-          this.getAll<IntegrationProfiles>('/conf/integration-profiles?type=PROXY_DEFINITION'),
-          this.getAll<Licenses>('/acq/licenses')
-        ])),
-        tap(results=>{
-          this._options.proxies = 
-            results[0].integration_profile.map(p=>({code: p.code, desc: p.code.concat(p.description ? ` (${p.description})` : '')}))
-            .sort(sort('desc'));
-          this._options.licenses = 
-            results[1].license.map(l=>({code: l.code, desc: l.name}))
-            .sort(sort('desc'));
+        switchMap(()=>forkJoin(
+          Object.values(this.otherApis).map(a=>this.getAll(a.uri))
+        )),
+        tap(results => {
+          Object.entries(this.otherApis).forEach(([k,v], i) => {
+            this._options[camelCase(k)] = 
+              findArray(results[i]).map(r=>({code: r[v.code], desc: v.desc(r)}))
+              .sort(sort('desc'));
+          });
         }),
         map(()=>this._options)
       )
@@ -67,7 +77,7 @@ export class OptionsService {
     req.queryParams['limit'] = chunkSize;
     return this.restService.call(req).pipe(
       tap(results => {
-        arrayName = arrayName || Object.keys(results).find(k=>Array.isArray(results[k]));
+        arrayName = arrayName || findArrayName(results);
         array = results[arrayName];
         count = results.total_record_count || 0;
       }),
@@ -77,7 +87,7 @@ export class OptionsService {
         forkJoin(
           arrayOf(Math.ceil(count/chunkSize)-1)
           .map(i=>{
-            const newReq = _.cloneDeep(req);
+            const newReq = cloneDeep(req);
             newReq.queryParams.offset = (i+1)*chunkSize;
             return this.restService.call(newReq);
           })
@@ -87,8 +97,8 @@ export class OptionsService {
             for (const result of results) {
               array = array.concat(result[arrayName]);
             }
-            return Object.assign(results[0], Object.assign(results[0], {[arrayName]: array})) as T
-;          })
+            return Object.assign(results[0], Object.assign(results[0], {[arrayName]: array})) as T;
+          })
         )
       ))
     )
@@ -102,3 +112,5 @@ const sort = (field: string) => ( a: any, b: any ) => {
 }
 
 const arrayOf = (length: number) => Array.from({length: length}, (v, i) => i);
+const findArray = (obj: Object) => obj[findArrayName(obj)] || [];
+const findArrayName = (obj: Object) => Object.keys(obj).find(k=>Array.isArray(obj[k]));
